6d7bfe25124b075f349ff2aad1a62eb63700e69c
Ralf <bubriks@gmail.com>
2021-11-08 20:21:44 +0100

    MinHashing

---
+++ Homework 1/min_hashing.py
@@ -0,0 +1,50 @@
+import random
+import sympy
+
+class MinHashing:
+    
+    def get_coefficients(self, k):
+        randList = []
+      
+        for i in range(k):
+            # Get a random shingle ID.
+            randIndex = random.randint(0, self.maxShingleID)
+            # Ensure that each random number is unique.
+            while randIndex in randList:
+                randIndex = random.randint(0, self.maxShingleID)
+            # Add the random number to the list.
+            randList.append(randIndex)
+
+        return randList
+
+    def get_signature(self, shingles: set):
+        signature = []
+        
+        #function will be: h(x) = (a*x + b) % c
+        for i in range(self.n):
+            minHashCode = self.c + 1
+            
+            for x in shingles:
+                hashCode = (self.a[i] * x + self.b[i]) % self.c
+                if hashCode < minHashCode:
+                    minHashCode = hashCode
+            
+            signature.append(minHashCode)
+
+        return signature
+
+
+    def __init__(self, n: int = 100):
+        self.n = n
+        
+        # Record the maximum shingle ID that we assigned (we used 32 bits).
+        self.maxShingleID = 2**32-1
+        self.c = sympy.nextprime(self.maxShingleID)
+
+        self.a = self.get_coefficients(n)
+        self.b = self.get_coefficients(n)
+
+# example usage
+m = MinHashing(2)
+result = m.get_signature([901544789, 2659403885, 3265866552])
+print(result)
--- Homework 1/shingling.py
+++ Homework 1/shingling.py
@@ -1,10 +1,14 @@
-class Shingling:
+import binascii
+
+class Shingling: 
 
     def __init__(self, text: str, k: int):
         self.shingles = set()
         text = text.lower()
         for i in range(len(text)+1-k):
-            hashed_value = hash(text[i:i+k])
+            shingle = text[i:i+k]
+            # Hash the shingle to a 32-bit integer.
+            hashed_value = binascii.crc32(bytes(shingle, 'utf-8')) & 0xffffffff
             self.shingles.add(hashed_value)
         self.shingles = sorted(self.shingles)
 
