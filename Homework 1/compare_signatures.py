840a4caecdad27421d93331439d5056322cb87ca
Ralf <bubriks@gmail.com>
2021-11-08 20:49:51 +0100

    Create compare_signatures.py

---
+++ Homework 1/compare_signatures.py
@@ -0,0 +1,14 @@
+class CompareSignatures:
+
+    def compare(signatures1, signatures2):
+        count = 0
+        zipped = list(zip(signatures1, signatures2))
+        for s1, s2 in zipped:
+            if s1 == s2:
+                count = count + 1
+
+        return count/len(zipped)
+
+# example usage
+result = CompareSignatures.compare([1,2,3,4], [3,2,5,6])
+print(result)
