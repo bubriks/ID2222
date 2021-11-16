1860bff516874cd1a7ce61519cd683105ea42683
Ralf <bubriks@gmail.com>
2021-11-16 18:41:54 +0100

    basic apriori

--- Homework 2/apriori.py
+++ Homework 2/apriori.py
@@ -2,55 +2,46 @@ import numpy
 import itertools
 import collections
 
-def read_groups():
+def read_baskets():
     with open("T10I4D100K.dat", "r") as f:
-    #with open("temp.dat", "r") as f: # for learning 
         lines = f.read().splitlines()
-        group_list = [list(set(i.strip().split(" "))) for i in lines] #list(set) to remove duplicates on the same line
+        group_list = [list(set(i.strip().split(" "))) for i in lines] #list(set) to remove duplicates on the basket (not interesting for us)
     return group_list
 
-def reduce(tuples, k):
-    results = list()
-    for i in tuples:
-        result = set(numpy.concatenate(i).flat)
-        if len(result) == k:
-            results.append(tuple(result))
-    return results
+def generate_candidates(frequent_itemset):
+    candidates = dict()
+    for frequent_itemset in frequent_itemset:
+        for singleton in singletons:
+            if singleton[0] not in frequent_itemset:
+                candidate = sorted(frequent_itemset + singleton)# to ensure that order doesnt matter
+                candidates[tuple(candidate)] = 0
+    return candidates
 
-groups = read_groups()
+def count_candidates(candidates, tuple_size):
+    for basket in baskets:
+        basket_variations = itertools.combinations(basket, tuple_size) #all variation for the tuple size
+        for combination in basket_variations:
+            if combination in candidates:
+                candidates[combination] += 1
+    return candidates
+
+baskets = read_baskets()
 
 frequent_itemsets = []
 support = 1000
 
 #singletons
-flat_baskets = list(numpy.concatenate(groups).flat)
-occurrences = collections.Counter(flat_baskets)
-chosen = [(value,) for value, count in occurrences.items() if count >= support]
-frequent_itemsets.append(chosen)
+candidates = collections.Counter(list(numpy.concatenate(baskets).flat))
+singletons = [(value,) for value, count in candidates.items() if count >= support]
+frequent_itemsets.append(singletons)
 
-#multitons
+#multitons (not sure if its a word :D )
 k = 0
 while len(frequent_itemsets[k]) > 1: # more than 1 element in the list
     tuple_size = k + 2
-    tuples = set(itertools.combinations(frequent_itemsets[k], tuple_size))
-    tuples = reduce(tuples, tuple_size)
-    
-    tp = len(tuples)
-    
-    chosen = []
-    i = 0
-    for t in tuples:
-        count = 0
-        for g in groups:
-            if set(t).issubset(set(g)):
-                count = count + 1
-        if count >= support:
-            chosen.append(t)
-        i += 1
-        print(f"\r{round((i/tp), 2)} %", flush=True, end="\r")
-    
-    frequent_itemsets.append(chosen)
-    k = k + 1
-    print(k)
-
+    candidates = generate_candidates(frequent_itemsets[k]) #make candidates
+    candidate_count = count_candidates(candidates, tuple_size) #count candidates
+    frequent_itemset = [i for i in candidate_count if candidate_count[i] >= support] #filter candidates
+    frequent_itemsets.append(frequent_itemset)#add frequent itemset
+    k += 1
 print(frequent_itemsets)
