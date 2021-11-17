2a2a214324ce9882aee6febe240ff706d9b87643
Ralf <bubriks@gmail.com>
2021-11-17 19:25:37 +0100

    Update apriori.py

--- Homework 2/apriori.py
+++ Homework 2/apriori.py
@@ -28,11 +28,11 @@ def count_candidates(candidates, tuple_size):
 baskets = read_baskets()
 
 frequent_itemsets = []
-support = 1000
+support = 700
 
 #singletons
 candidates = collections.Counter(list(numpy.concatenate(baskets).flat))
-singletons = [(value,) for value, count in candidates.items() if count >= support]
+singletons = {(value,): count for value, count in candidates.items() if count >= support}
 frequent_itemsets.append(singletons)
 
 #multitons (not sure if its a word :D )
@@ -41,7 +41,35 @@ while len(frequent_itemsets[k]) > 1: # more than 1 element in the list
     tuple_size = k + 2
     candidates = generate_candidates(frequent_itemsets[k]) #make candidates
     candidate_count = count_candidates(candidates, tuple_size) #count candidates
-    frequent_itemset = [i for i in candidate_count if candidate_count[i] >= support] #filter candidates
+    frequent_itemset = {i: candidate_count[i] for i in candidate_count if candidate_count[i] >= support} #filter candidates
     frequent_itemsets.append(frequent_itemset)#add frequent itemset
     k += 1
 print(frequent_itemsets)
+
+
+
+
+c = 0.6
+
+def get_confidence(frequent_itemset, effect):
+    frequent_itemset_support = frequent_itemsets[len(tuple_permutation) - 1][frequent_itemset]
+    effect_support = frequent_itemsets[len(effect) - 1][effect]
+    return round(frequent_itemset_support / effect_support, 2)
+
+for frequent_itemset in frequent_itemsets[1:]:# anything with more than 1 element in the tuple
+    for k_tuple in frequent_itemset:
+        all_possible_effect_sizes = range(1, len(k_tuple))# all possible orders of implication for the tuple A -> B, C; A,B -> C
+        seen_effect = list()
+        for tuple_permutation in itertools.permutations(k_tuple, len(k_tuple)):# all possible permutations of the tuple
+            for effect_size in reversed(all_possible_effect_sizes):
+                effect = tuple(sorted(tuple_permutation[:effect_size]))
+                if effect in seen_effect:# continue if such a combinations was considered for this tuple
+                    continue
+                seen_effect.append(effect)
+                confidence = get_confidence(tuple(sorted(tuple_permutation)), effect)
+                if confidence >= c:
+                    effect = tuple_permutation[:effect_size]
+                    result = tuple_permutation[effect_size:]
+                    print(f"{effect} -> {result} : {confidence}%")
+                else:
+                    break # If A,B,C→D is below confidence, so is A,B→C,D because of supp(A,B) ≥ supp(A,B,C)
