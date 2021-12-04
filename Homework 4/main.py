0fe4c2f1ef97d17a28adf255a753c58671090358
Ralf <bubriks@gmail.com>
2021-12-04 10:24:33 +0100

    Update main.py

--- Homework 4/main.py
+++ Homework 4/main.py
@@ -1,10 +1,8 @@
-import pandas as pd
 import os
 import numpy as np
 import networkx as nx
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
-from scipy.spatial import distance
 
 filename = os.path.join(os.getcwd(), "Homework 4", "example1.dat")
 
@@ -14,20 +12,17 @@ class SpectralClustering:
         self.k = k
         pass
 
-    # def CalculateValue(self, si, sj) :
-    #     return np.exp(-(np.square(si-sj))/(2*np.square(self.std)))
-    
-    def AffinityMatrix(self, graph) :
+    def AffinityMatrix(self, graph):
         # make adjcent matrix
         A = np.zeros([self.nodes, self.nodes])
-        for edge in graph.edges :
+        for edge in graph.edges:
             if edge[0] != edge[1] :
                 # non-direction graph : symmetric
                 A[edge[0]-1][edge[1]-1] = 1
                 A[edge[1]-1][edge[0]-1] = 1
         return A
-        
-    def DiagonalMatrix(self, affinityM) :
+
+    def DiagonalMatrix(self, affinityM):
         D_sqr_root = np.sqrt(np.diag(np.sum(affinityM, axis=1)))
         D_sqr_root_inv = np.linalg.inv(D_sqr_root)
         L = np.dot(np.dot(D_sqr_root_inv, affinityM), D_sqr_root_inv)
@@ -37,22 +32,22 @@ class SpectralClustering:
         value, eigenVectors = np.linalg.eigh(L) # returned eigenvectors are sorted in accending
         return eigenVectors[:, -self.k:] # K_th largest vector
 
-    def ReNormalizing(self, X) :
+    def ReNormalizing(self, X):
         Y = np.zeros_like(X)
-        for i, row in enumerate(X) :
+        for i, row in enumerate(X):
             x_sqr = np.dot(row, row.T)
             Y[i] = row/np.sqrt(x_sqr)
         return Y
-    
+
     def KmeanClustering(self, Y):
         return KMeans(n_clusters=self.k).fit(Y).labels_
 
-def saveplot(filename) :
+def saveplot(filename):
     savename = os.path.join(os.getcwd(), 'Homework 4', filename+'.png')
     plt.savefig(savename)
 
 
-if __name__=="__main__" :
+if __name__=="__main__":
     edge_list = nx.read_edgelist(filename, delimiter=",", nodetype=int, data=(("weight", int), ))
     graph = nx.Graph()
     graph.add_nodes_from(sorted(edge_list.nodes.keys()))
@@ -76,11 +71,8 @@ if __name__=="__main__" :
     Y = sc.ReNormalizing(X)
     # 5. K-means clustering
     labels = sc.KmeanClustering(Y)
-   
+
     plt.figure()
     nx.draw(graph, node_size=20, node_color=labels)
     saveplot('after_cluster')
     plt.close()
-
-
-     
