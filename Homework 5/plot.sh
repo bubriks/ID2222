e0a716ea91cbb177158c20f2cb3090dad40854c2
Ralf <bubriks@gmail.com>
2021-12-07 18:57:31 +0100

    pause added to sh file

--- Homework 5/plot.sh
+++ Homework 5/plot.sh
@@ -11,3 +11,4 @@ fi
 gnuplot -e "filename='$1'" graph.gnuplot
 
 xdg-open graph.png
+read -p "Press any key to resume ..."
--- Homework 5/run.sh
+++ Homework 5/run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 java -Xmx5000m -jar target/assignment4-jabeja-1.0-jar-with-dependencies.jar $@
+read -p "Press any key to resume ..."
